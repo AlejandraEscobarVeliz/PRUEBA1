4f017431f7b7c6d606d678aef197b91af21a3e79
AlejandraEscobarVeliz <alejandra.escobar@ucb.edu.bo>
2025-09-05 08:43:02 -0400

    Pruebita

---
+++ PRUEBA1_Ale/Controllers/ValuesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PRUEBA1_Ale.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+    }
+}
